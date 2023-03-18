4839f45b25008aab94d47807d4812e25dc3af632
Ipromisee <126790648+Ipromisee@users.noreply.github.com>
2023-03-18 22:36:10 +0800

    Add files via upload

---
+++ Assignment4/Clock/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public delegate void TickHandler(object sender, DateTime time);
+public delegate void AlarmHandler(object sender, DateTime time);
+
+//publisher
+class ClockRun
+{
+    public event TickHandler OnTick;
+    public event AlarmHandler OnAlarm;
+    public string alarmTime;// 闹钟时间
+
+    public void runtime()//时间进行
+    {
+        while (true)
+        {
+            DateTime now = DateTime.Now;
+            OnTick(this, now);
+            if (now.ToString() == alarmTime)
+            {
+                OnAlarm(this, now);
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
+
+//Subscriber
+class Clock
+{
+    public ClockRun clk = new ClockRun();
+    public Clock(string alarm)
+    {
+        clk.alarmTime = alarm;
+        clk.OnTick += onTick;
+        clk.OnAlarm += onAlarm;
+    }
+    void onTick(object sender, DateTime time)
+    {
+        Console.WriteLine($"嘀嗒嘀嗒，现在是{time}。");
+    }
+    void onAlarm(object sender, DateTime time)
+    {
+        Console.WriteLine($"叮铃铃，{time}了，要迟到啦。");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clock ck = new Clock("2023/3/18 17:15:40");
+        ck.clk.runtime();
+    }
+}
---
+++ Assignment4/List/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+//链表节点
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+// 泛型链表类
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        head = tail = null;
+    }
+
+    public Node<T> Head { get => head; }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        for(Node<T> cur = head; cur!=tail; cur=cur.Next)
+        {
+            action(cur.Data);
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0, min = int.MaxValue, max = int.MinValue;
+        GenericList <int> intList= new GenericList<int>();
+        int[] a = { 1, 2, 3, 4, 5 };
+        //根据数组a创建一个链表
+        for(int i = 0; i < a.Length; i++) { intList.Add(a[i]); }
+        // 用lambda表达式，=>前面的是形参
+        intList.ForEach(data => Console.Write(data+" "));//输出所有元素
+        intList.ForEach(data => max = Math.Max(max, data));//找到最大值
+        intList.ForEach(data => min = Math.Min(min, data));//找到最小值
+        intList.ForEach(data => sum += data);//求和
+        Console.WriteLine();
+        Console.WriteLine($"最大值：{max},最小值：{min},和：{sum}");
+    }
+}
